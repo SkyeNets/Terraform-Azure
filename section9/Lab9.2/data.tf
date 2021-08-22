f0344728688eca655918098e7fd23f2e95e53c82
Roger2kTaylor <roger@skye-nets.com>
2021-08-22 17:58:39 -0400

    removed copies added 9.3

--- section9/Lab9.2/data.tf
+++ section9/Lab9.2/data.tf
@@ -1,8 +1,8 @@
 # Data 
 
 data "azurerm_key_vault" "skye-vault" {
-  name                = "skye-vault-001"
-  resource_group_name = "SkyeLab-rg"
+  name                = "skye-vault-02"
+  resource_group_name = "skyelab-env-rg"
 }
 
 data "azurerm_key_vault_secret" "skye-vault" {
