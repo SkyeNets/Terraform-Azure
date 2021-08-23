edef9c89fd561918d284ecd7c266ffc008127978
Roger2kTaylor <roger@skye-nets.com>
2021-08-22 21:36:48 -0400

    Lab 10.1 code updated

---
+++ section10/Lab10.1/backend.tf
@@ -0,0 +1,13 @@
+# Please use terraform v12.29 to start with for all labs, I will use terraform v13 and v14 from lab 7.5 onwards
+
+
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "skyelab-env-rg"
+    storage_account_name = "skyeremotesa02"
+    container_name       = "tfstate"
+  }
+  
+}
+
---
+++ section10/Lab10.1/provider.tf
@@ -0,0 +1,17 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Please use terraform v12.29 to start with for all labs, I will use terraform v13 and v14 from lab 7.5 onwards
+provider "azurerm" {
+  #  version = "= 2.18"
+  features {}
+
+}
